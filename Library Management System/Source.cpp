61aaf01e1ad7e4998d4e688f60ae519346451068
Menna Mohamed <2022170446@cis.asu.edu.eg>
2023-03-28 16:33:38 +0200

    add description

--- Library Management System/Source.cpp
+++ Library Management System/Source.cpp
@@ -23,6 +23,8 @@ struct book
 
 int main()
 {
-	fantasy[0] = { "Red Queen", "Victoria Aveyard"," ", "hardcover", "available", 7.98, 388, 4.02 };
-	cout << fantasy[0].title;
+	fantasy[0] = { "Red Queen", "Victoria Aveyard"
+					,"The Reds are commoners, ruled by a Silver elite in possession of god-like superpowers. And to Mare Barrow, a seventeen-year-old Red girl from the poverty-stricken Stilts, it seems like nothing will ever change.\n\nThat is until she finds herself working in the Silver Palace. Here, surrounded by the people she hates the most, Mare discovers that, despite her red blood, she possesses a deadly power of her own. One that threatens to destroy the balance of power.\n\nFearful of Mare's potential, the Silvers hide her in plain view, declaring her a long-lost Silver princess, now engaged to a Silver prince. Despite knowing that one misstep would mean her death, Mare works silently to help the Red Guard, a militant resistance group, and bring down the Silver regime.\n\nBut this is a world of betrayal and lies, and Mare has entered a dangerous dance—Reds against Silvers, prince against prince, and Mare against her own heart.\n\n"
+					, "hardcover", "available", 7.98, 388, 4.02 };
+	cout << fantasy[0].discription;
 }
