da395567ce79fea81eb84d8847441508d2c9c808
mariamelkondakly <111656299+mariamelkondakly@users.noreply.github.com>
2023-03-28 16:11:03 +0200

    added branch book struct and typed a code sample.

--- Library Management System/Source.cpp
+++ Library Management System/Source.cpp
@@ -3,7 +3,26 @@
 #include <fstream>
 using namespace std;
 
+const int arrsize = 10; 
+
+//array size of books
+//type is whether book is digital, paperback or hardcover
+
 struct book
 {
-	
-};
+	string title;
+	string author;
+	string discription;
+	string type;
+	string status;
+	float price;
+	int numofpages;
+	float review;
+
+} fantasy[arrsize], action[arrsize], romcom[arrsize], nonfiction[arrsize], scifi[arrsize];
+
+int main()
+{
+	fantasy[0] = { "Red Queen", "Victoria Aveyard"," ", "hardcover", "available", 7.98, 388, 4.02 };
+	cout << fantasy[0].title;
+}
