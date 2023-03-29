3aa71186102ecfe09346d968e8d9b033c0224df6
Menna Mohamed <2022170446@cis.asu.edu.eg>
2023-03-30 00:50:37 +0200

    from files to struct

--- Library Management System/Source.cpp
+++ Library Management System/Source.cpp
@@ -3,10 +3,12 @@
 #include <fstream>
 using namespace std;
 
-const int arrsize = 10000000; 
+const int arrsize = 10000; 
 
 //array size of books
 //type is whether book is digital, paperback or hardcover
+
+void books_to_struct();
 
 struct book
 {
@@ -15,13 +17,41 @@ struct book
 	string discription;
 	string type;
 	string status;
-	float price;
-	int numofpages;
-	float review;
+	string price;
+	string numofpages;
+	string review;
 
 } fantasy[arrsize], action[arrsize], romcom[arrsize], non_fiction[arrsize], scientific_fiction[arrsize];
 
+
 int main()
 {
+	books_to_struct();
+	cout << fantasy[1].title << "\n\n";
+	return 0;
+}
 
+void books_to_struct() {
+	string line;
+	int index;
+	ifstream file("books.txt");
+
+	if (file.is_open()) {
+		for (int i = 0;i < 2;i++) {
+			index = 0;
+			while (getline(file, line))
+			{
+				if (index == 0)    fantasy[i].title = line;
+				else if (index == 1)    fantasy[i].author = line;
+				else if (index == 2)    fantasy[i].discription = line;
+				else if (index == 3)    fantasy[i].type = line;
+				else if (index == 4)    fantasy[i].status = line;
+				else if (index == 5)    fantasy[i].price = line;
+				else if (index == 6)    fantasy[i].numofpages = line;
+				else if (index == 7)    fantasy[i].review = line;
+				index++;
+				if (index == 8)	break;
+			}
+		}
+	}
 }
