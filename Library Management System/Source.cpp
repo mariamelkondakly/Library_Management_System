dedb4cbc446432116c915ea9d8d7b2280e5988d0
MostafaMohamed2005 <mostafa.mosael13@gmail.com>
2023-04-04 20:19:11 +0200

    Sections

--- Library Management System/Source.cpp
+++ Library Management System/Source.cpp
@@ -1,112 +1,367 @@
 #include <iostream>
-#include<string>
+#include <string>
 #include <fstream>
 using namespace std;
+
 const int arrsize = 10000;
+int fantasy_num = 10, mystery_num = 11, romance_num = 10, non_fiction_num = 10, science_fiction_num = 10;
+int fantasycounter = 0, mysterycounter = 0, romancecounter = 0, non_fictioncounter = 0, science_fictioncounter = 0;
 
-int number, section = 0, number_of_books = 0; int answers;
-void empty() {
-    cout << "\033[2J\033[1;1H";
-};
-
-struct cart {
-    string title;
-    string status;
-    float price;
-}reader[1000];
+//array size of books
+//type is whether book is digital, paperback or hardcover
 
 struct book
 {
-    string title;
-    string author;
-    string discription;
-    string type;
-    string status;
-    string price;
-    string numofpages;
-    string review;
+	string title;
+	string author;
+	string discription;
+	string type;
+	string status;
+	int price;
+	string numofpages;
+	string review;
 
 } fantasy[arrsize], mystery[arrsize], romance[arrsize], non_fiction[arrsize], science_fiction[arrsize];
 
-char answer = 'y';
+void file_to_fantasy()
+//converting file to the fantasy struct
+{
+	string line;
+	int index;
+	ifstream file("fantasy.txt");
 
-void check(int number) {
-    do {
-        cin >> number;  // number = index + 1 of books in certain array
-        number_of_books++;
-        cout << "do you want to add another book? y/n \t";
-        cin >> answer;
-        if (answer == 'n' || answer == 'N')
-            break;
-        else if (answer == 'y' || answer == 'Y')
-            continue;
-        else
-            cout << "invalid option";
-    } while (answer != 'n');
+	if (file.is_open()) {
+		for (int i = 0; i < fantasy_num; i++) {
+			index = 0;
+			fantasycounter++;
+			while (getline(file, line))
+			{
+				if (index == 0)    fantasy[i].title = line;
+				else if (index == 1)    fantasy[i].author = line;
+				else if (index == 2)    fantasy[i].discription = line;
+				else if (index == 3)    fantasy[i].type = line;
+				else if (index == 4)    fantasy[i].status = line;
+				else if (index == 5)    fantasy[i].price = stoi(line);
+				else if (index == 6)    fantasy[i].numofpages = line;
+				else if (index == 7)    fantasy[i].review = line;
+				index++;
+				if (line == "##") { fantasycounter++; break; }
+				if (index == 8)		break;
+			}
+		}
+	}
+	file.close();
 }
 
-void file_to_cart()
+void file_to_mystery()
+//converting file to the mystery struct
 {
-        string line;
-        int index;
-        ifstream cart("cart.txt");
-        if (cart.is_open()) {
-            for (int i = 0; i < number_of_books; i++) {
-                index = 0;
-                while (getline(cart, line)) {
-                    reader[index].title = line;
-                    getline(cart, line);
-                    reader[index].status = line;
-                    getline(cart, line);
-                    reader[index].price = stof(line);
-                    index++;
-                }
-            }
-            number_of_books = index;
-            cart.close();
-        }
- }
+	string line;
+	int index;
+	ifstream file("mystery.txt");
 
-int main() {
-    file_to_cart();
-    int section;
-    cout << "which section,would you like to choose?" << endl << "1.fantasy\n" << "2.mystery\n" << "3.romance\n" << "4.non_fiction\n" << "5.science_fiction\n";
-    cin >> section;
-    switch (section) {
-    case 1:
-        check(number);    // number is index + 1 of section arrays 
-        break;
-    case 2:
-        check(number);
-        break;
-    case 3:
-        check(number);
-        break;
-    case 4:
-        check(number);
-        break;
-    case 5:
-        check(number);
-        break;
+	if (file.is_open()) {
+		for (int i = 0; i < mystery_num; i++) {
+			index = 0;
+			mysterycounter++;
+			while (getline(file, line))
+			{
+				if (index == 0)    mystery[i].title = line;
+				else if (index == 1)    mystery[i].author = line;
+				else if (index == 2)    mystery[i].discription = line;
+				else if (index == 3)    mystery[i].type = line;
+				else if (index == 4)    mystery[i].status = line;
+				else if (index == 5)    mystery[i].price = stoi(line);
+				else if (index == 6)    mystery[i].numofpages = line;
+				else if (index == 7)    mystery[i].review = line;
+				index++;
+				if (line == "##") { mysterycounter++; break; }
+				if (index == 8)	break;
+			}
+		}
+	}
 
-    }
-    cout << "would you like to :" << endl << "1.see your cart" << endl << "2.go back to your home page";
-    cin >> answers;
-    if (answers == 1) {
-        empty();
-        for (int i = 0; i < number_of_books; i++) {
-            cout << "book" << i + 1 << ':' << reader[i].title << endl;
-            cout << "price:" << reader[i].price;
-        }
+	file.close();
+}
+
+void file_to_romance()
+//converting file to the romance struct
+{
+	string line;
+	int index;
+	ifstream file("romance.txt");
+
+	if (file.is_open()) {
+		for (int i = 0; i < romance_num; i++) {
+			index = 0;
+			romancecounter++;
+			while (getline(file, line))
+			{
+				if (index == 0)    romance[i].title = line;
+				else if (index == 1)    romance[i].author = line;
+				else if (index == 2)    romance[i].discription = line;
+				else if (index == 3)    romance[i].type = line;
+				else if (index == 4)    romance[i].status = line;
+				else if (index == 5)    romance[i].price = stoi(line);
+				else if (index == 6)    romance[i].numofpages = line;
+				else if (index == 7)    romance[i].review = line;
+				index++;
+				if (line == "##") { romancecounter++; break; }
+				if (index == 8)	break;
+			}
+		}
+	}
+
+	file.close();
+}
+
+void file_to_non_fiction()
+//converting file to the non fiction struct
+{
+	string line;
+	int index;
+	ifstream file("nonfiction.txt");
+
+	if (file.is_open()) {
+		for (int i = 0; i < non_fiction_num; i++) {
+			index = 0;
+			non_fictioncounter++;
+			while (getline(file, line))
+			{
+				if (index == 0)    non_fiction[i].title = line;
+				else if (index == 1)    non_fiction[i].author = line;
+				else if (index == 2)    non_fiction[i].discription = line;
+				else if (index == 3)    non_fiction[i].type = line;
+				else if (index == 4)    non_fiction[i].status = line;
+				else if (index == 5)    non_fiction[i].price = stoi(line);
+				else if (index == 6)    non_fiction[i].numofpages = line;
+				else if (index == 7)    non_fiction[i].review = line;
+				index++;
+				if (line == "##") { non_fictioncounter++; break; }
+				if (index == 8)	break;
+			}
+		}
+	}
+	file.close();
+}
+
+void file_to_science_fiction()
+//converting file to the science fiction struct
+{
+	string line;
+	int index;
+	ifstream file("science_fiction.txt");
+
+	if (file.is_open()) {
+		for (int i = 0; i < science_fiction_num; i++) {
+			index = 0;
+			science_fictioncounter++;
+			while (getline(file, line))
+			{
+				if (index == 0)    science_fiction[i].title = line;
+				else if (index == 1)    science_fiction[i].author = line;
+				else if (index == 2)    science_fiction[i].discription = line;
+				else if (index == 3)    science_fiction[i].type = line;
+				else if (index == 4)    science_fiction[i].status = line;
+				else if (index == 5)    science_fiction[i].price = stoi(line);
+				else if (index == 6)    science_fiction[i].numofpages = line;
+				else if (index == 7)    science_fiction[i].review = line;
+				index++;
+				if (line == "##") { science_fictioncounter++; break; }
+				if (index == 8)	break;
+			}
+		}
+	}
+
+	file.close();
+}
+
+void struct_to_fantasy()
+{
+	ofstream file;
+	file.open("fantasy.txt");
+
+	for (int i = 0; i < fantasy_num; i++)
+	{
+
+		for (int j = 0; j < 8; j++)
+		{
+			if (j == 0) file << fantasy[i].title << endl;
+			else if (j == 1) file << fantasy[i].author << endl;
+			else if (j == 2) file << fantasy[i].discription << endl;
+			else if (j == 3) file << fantasy[i].type << endl;
+			else if (j == 4) file << fantasy[i].status << endl;
+			else if (j == 5) file << fantasy[i].price << endl;
+			else if (j == 6) file << fantasy[i].numofpages << endl;
+			else if (j == 7) file << fantasy[i].review << endl;
+		}
+	}
+	file << "##";
+	file.close();
+}
+
+void struct_to_mystery()
+{
+	ofstream file;
+	file.open("mystery.txt");
+
+	for (int i = 0; i < mystery_num; i++)
+	{
+
+		for (int j = 0; j < 8; j++)
+		{
+			if (j == 0) file << mystery[i].title << endl;
+			else if (j == 1) file << mystery[i].author << endl;
+			else if (j == 2) file << mystery[i].discription << endl;
+			else if (j == 3) file << mystery[i].type << endl;
+			else if (j == 4) file << mystery[i].status << endl;
+			else if (j == 5) file << mystery[i].price << endl;
+			else if (j == 6) file << mystery[i].numofpages << endl;
+			else if (j == 7) file << mystery[i].review << endl;
+		}
+	}
+	file << "##";
+	file.close();
+}
+
+void struct_to_romantic()
+{
+	ofstream file;
+	file.open("romance.txt");
+
+	for (int i = 0; i < romance_num; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (j == 0) file << romance[i].title << endl;
+			else if (j == 1) file << romance[i].author << endl;
+			else if (j == 2) file << romance[i].discription << endl;
+			else if (j == 3) file << romance[i].type << endl;
+			else if (j == 4) file << romance[i].status << endl;
+			else if (j == 5) file << romance[i].price << endl;
+			else if (j == 6) file << romance[i].numofpages << endl;
+			else if (j == 7) file << romance[i].review << endl;
+		}
+	}
+	file << "##";
+	file.close();
+}
 
 
-    }
-    else if (answer == 2) {
-        empty();
-        //menu
+void struct_to_non_fiction()
+{
+	ofstream file;
+	file.open("non_fiction.txt");
 
-    }
+	for (int i = 0; i < non_fiction_num; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (j == 0) file << non_fiction[i].title << endl;
+			else if (j == 1) file << non_fiction[i].author << endl;
+			else if (j == 2) file << non_fiction[i].discription << endl;
+			else if (j == 3) file << non_fiction[i].type << endl;
+			else if (j == 4) file << non_fiction[i].status << endl;
+			else if (j == 5) file << non_fiction[i].price << endl;
+			else if (j == 6) file << non_fiction[i].numofpages << endl;
+			else if (j == 7) file << non_fiction[i].review << endl;
+		}
+	}
+	file << "##";
+	file.close();
+}
+
+void struct_to_scientific_fiction()
+{
+	ofstream file;
+	file.open("science_fiction.txt");
+
+	for (int i = 0; i < science_fiction_num; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (j == 0) file << science_fiction[i].title << endl;
+			else if (j == 1) file << science_fiction[i].author << endl;
+			else if (j == 2) file << science_fiction[i].discription << endl;
+			else if (j == 3) file << science_fiction[i].type << endl;
+			else if (j == 4) file << science_fiction[i].status << endl;
+			else if (j == 5) file << science_fiction[i].price << endl;
+			else if (j == 6) file << science_fiction[i].numofpages << endl;
+			else if (j == 7) file << science_fiction[i].review << endl;
+		}
+	}
+	file << "##";
+	file.close();
+}
+
+void files_to_struct() {
+	file_to_fantasy();
+	file_to_mystery();
+	file_to_romance();
+	file_to_non_fiction();
+	file_to_science_fiction();
+}
+void structs_to_files()
+{
+	struct_to_fantasy();
+	struct_to_mystery();
+	struct_to_romantic();
+	struct_to_non_fiction();
+	struct_to_scientific_fiction();
+}
+
+void sections()
+{
+	int section_num;
+
+	cout << "1 - Fantasy\n" << "2 - Mystery\n" << "3 - Romantic\n" << "4 - Non fiction\n" << "5 - Scientific fiction\n";
+	cout << "Please enter the number of the section : ";
+	cin >> section_num;
+
+	if (section_num == 1)
+	{
+		for (int i = 0; i < fantasy_num; i++)
+		cout << i + 1 << " - " << fantasy[i].title << endl;
+	}
 
 
-    return 0;
+	else if (section_num == 2)
+	{
+		for (int i = 0; i < mystery_num; i++)
+		cout << i + 1 << " - " << mystery[i].title << endl;
+	}
+
+	else if (section_num == 3)
+	{
+		for (int i = 0; i < romance_num; i++)
+		cout << i + 1 << " - " << romance[i].title << endl;
+	}
+
+	else if (section_num == 4)
+	{
+		for (int i = 0; i < non_fiction_num; i++)
+		cout << i + 1 << " - " << non_fiction[i].title << endl;
+	}
+
+	else if (section_num == 5)
+	{
+		for (int i = 0; i < science_fiction_num; i++)
+		cout << i + 1 << " - " << science_fiction[i].title << endl;
+	}
+
+	else
+	{
+		cout << "Invalid Section\n";
+		sections();
+	}
+
+
+}
+int main()
+{
+	files_to_struct();
+	for (int i = 0; i < 9; i++)
+	cout << non_fiction[i].title << endl;
+	structs_to_files();
+	return 0;
 }
